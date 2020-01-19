ae339372325780eb48e92457d37fd4b661440727
villainhr <villainthr@gmail.com>
2020-01-19 19:46:05 +0800

    feat: update

--- kboneui/src/router/routeConfig.js
+++ kboneui/src/router/routeConfig.js
@@ -1,16 +1,19 @@
 export default {
-    index:"/",
-    pages:{
-        flex:"/flex",
-        form:"/form",
-        slider:"/slider",
-        button:"/button",
-        actionsheet:"/actionsheet",
-        dialog:"/dialog",
-        toast:"/toast",
-        progress:"/progress",
-        toptips:"/toptips",
-        picker:"/picker",
-        swiper:"/swiper"
+    index: '/',
+    pages: {
+        flex: '/flex',
+        scrollview: '/scrollview',
+        form: '/form',
+        slider: '/slider',
+        button: '/button',
+        actionsheet: '/actionsheet',
+        dialog: '/dialog',
+        toast: '/toast',
+        progress: '/progress',
+        toptips: '/toptips',
+        picker: '/picker',
+        swiper: '/swiper',
+        cells: '/cells',
+        movable: '/movable',
     }
 }
--- kboneui/src/view/components/form/slider.vue
+++ kboneui/src/view/components/form/slider.vue
@@ -1,16 +1,34 @@
 <template>
-    <KView class="page slider js_show">
-        <KView class="page__hd">
-            <h1 class="page__title">Slider</h1>
-            <p class="page__desc">滑块</p>
-        </KView>
-
-        <KView class="page__bd page__bd_spacing">
-            <KSlider/>
-            <KSlider :show-value="true" />
-        </KView>
-
+  <KView class="page slider js_show">
+    <KView class="page__hd">
+      <h1 class="page__title">Slider</h1>
+      <p class="page__desc">滑块</p>
     </KView>
 
+    <KView class="page__bd page__bd_spacing">
+      <KSlider/>
+      <KSlider
+        :show-value="true"
+        v-model="slider1"
+        @change="change" />
+    </KView>
+
+  </KView>
+
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            slider1: 10
+        }
+    },
+    methods: {
+        change() {
+            console.log(this.slider1)
+        }
+    }
+}
+</script>
 
